ba1a21769b18b991f6aaed9b83ea75b0feea0eb1
Saull Brandão <saull@outlook.com>
2021-07-08 11:49:29 -0300

    use tdd to create Editor component

---
+++ src/__tests__/10-tdd-04-router-redirect.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import {Redirect as MockRedirect} from 'react-router'
+import userEvent from '@testing-library/user-event'
+import {savePost as mockSavePost} from '../api'
+import {Editor} from '../post-editor-04-router-redirect'
+
+jest.mock('react-router', () => {
+  return {
+    Redirect: jest.fn(() => null),
+  }
+})
+jest.mock('../api')
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+test('renders a form with title, content, tags and a submit button', async () => {
+  mockSavePost.mockResolvedValueOnce()
+  const fakeUser = {id: 'fake-user'}
+  render(<Editor user={fakeUser} />)
+  const fakePost = {
+    title: 'Test title',
+    content: 'Test content',
+    tags: ['tag1', 'tag2'],
+  }
+  screen.getByLabelText(/title/i).value = fakePost.title
+  screen.getByLabelText(/content/i).value = fakePost.content
+  screen.getByLabelText(/tags/i).value = fakePost.tags.join(', ')
+  const submitButton = screen.getByText(/submit/i)
+
+  userEvent.click(submitButton)
+  expect(submitButton).toBeDisabled()
+
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    authorId: fakeUser.id,
+  })
+
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
+
+  await waitFor(() => expect(MockRedirect).toHaveBeenCalledWith({to: '/'}, {}))
+})
---
+++ src/__tests__/10-tdd-05-dates.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import {Redirect as MockRedirect} from 'react-router'
+import userEvent from '@testing-library/user-event'
+import {savePost as mockSavePost} from '../api'
+import {Editor} from '../post-editor-05-dates'
+
+jest.mock('react-router', () => {
+  return {
+    Redirect: jest.fn(() => null),
+  }
+})
+jest.mock('../api')
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+test('renders a form with title, content, tags and a submit button', async () => {
+  mockSavePost.mockResolvedValueOnce()
+  const fakeUser = {id: 'fake-user'}
+  render(<Editor user={fakeUser} />)
+  const fakePost = {
+    title: 'Test title',
+    content: 'Test content',
+    tags: ['tag1', 'tag2'],
+  }
+  const preDate = new Date().getTime()
+
+  screen.getByLabelText(/title/i).value = fakePost.title
+  screen.getByLabelText(/content/i).value = fakePost.content
+  screen.getByLabelText(/tags/i).value = fakePost.tags.join(', ')
+  const submitButton = screen.getByText(/submit/i)
+
+  userEvent.click(submitButton)
+  expect(submitButton).toBeDisabled()
+
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    date: expect.any(String),
+    authorId: fakeUser.id,
+  })
+
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
+
+  const postDate = new Date().getTime()
+  const date = new Date(mockSavePost.mock.calls[0][0].date).getTime()
+  expect(date).toBeGreaterThanOrEqual(preDate)
+  expect(date).toBeLessThanOrEqual(postDate)
+
+  await waitFor(() => expect(MockRedirect).toHaveBeenCalledWith({to: '/'}, {}))
+})
