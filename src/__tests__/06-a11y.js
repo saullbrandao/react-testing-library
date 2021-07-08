e050f7a1ef397ddf7cea7d492ad8d8a171afaa5b
Saull Brandão <saull@outlook.com>
2021-07-08 09:42:09 -0300

    test accessibility using axe

---
+++ src/__tests__/06-a11y.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import {axe} from 'jest-axe'
+
+function Form() {
+  return (
+    <form>
+      <label htmlFor="email">Email</label>
+      <input id="email" placeholder="email" />
+    </form>
+  )
+}
+
+test('the form is accessible', async () => {
+  const {container} = render(<Form />)
+  expect(await axe(container)).toHaveNoViolations()
+})
